e7cd034f1b7310bce0c84018b1d7da96fa4d15ab
Alexander Evstratov <alexdrugev@me.com>
2024-03-28 10:04:55 +0300

    Add tutorial.

--- Assets/_Scripts/PlayerShoot.cs
+++ Assets/_Scripts/PlayerShoot.cs
@@ -7,8 +7,11 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _circlePrefab;
 
+    [SerializeField] private GameObject _tapToShotTutorial;
+
     public void ShootButton()
     {
+        _tapToShotTutorial.SetActive(false);
         GameObject circlePrefab = Instantiate(_circlePrefab);
         circlePrefab.transform.position = _player.transform.position;
         circlePrefab.transform.rotation = _player.transform.rotation;
