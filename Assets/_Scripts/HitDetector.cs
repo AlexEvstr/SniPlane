f4222cebcb5c3ed634d2830ca8cd6c1b68c5fb88
Alexander Evstratov <alexdrugev@me.com>
2024-03-28 09:56:06 +0300

    Add target behavior and spawn.

---
+++ Assets/_Scripts/HitDetector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetector : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            Destroy(collision.gameObject);
+            GameObject newTarget = Instantiate(_target);
+            _target.transform.position = new Vector2(Random.Range(-2, 2), Random.Range(1, 4));
+            Destroy(gameObject);
+        }
+    }
+}
